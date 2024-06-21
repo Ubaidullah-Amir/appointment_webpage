a18a41aeffa71d28687918cc81a07ae43671ef20
Ubaidullah-Amir <ubaidullahamir2017@gmail.com>
2024-06-21 00:53:25 -0700

    appointment is successfully created using the webpage

--- controllers/main.py
+++ controllers/main.py
@@ -1,6 +1,7 @@
 # appointment_webpage/controllers/main.py
 from odoo import http
 from odoo.http import request
+from datetime import datetime
 
 class AppointmentWebpage(http.Controller):
 
@@ -10,20 +11,28 @@ class AppointmentWebpage(http.Controller):
         patients = request.env['om_hospital.pateint'].search([])
         return request.render('appointment_webpage.appointment_form', {
             'doctors': doctors,
-            'patients': patients
+            'patients': patients,
         })
 
     @http.route('/create/appointment', type='http', auth="public", methods=['POST'], website=True)
     def create_appointment(self, **post):
+        # Convert the appointment_time from form to Odoo's expected format
+        appointment_time_str = post.get('appointment_time')
+        if appointment_time_str:
+            appointment_time = datetime.strptime(appointment_time_str, '%Y-%m-%dT%H:%M')
+            appointment_time = appointment_time.strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            appointment_time = None
+        
+        # Create the appointment record
         request.env['om_hospital.appointment'].sudo().create({
             'ref': post.get('ref'),
-            'prescription': post.get('prescription'),
+            'note': post.get('note'),
             'priority': post.get('priority'),
             'doctor_id': int(post.get('doctor_id')),
             'pateint_id': int(post.get('pateint_id')),
-            'appointment_time': post.get('appointment_time'),
+            'appointment_time': appointment_time,
             'booking_Date': post.get('booking_Date'),
-            'note': post.get('note'),
             'state': 'draft'
         })
         return request.redirect('/appointment')
